6007d2c5506feaa34ca45d0182b5c0a9704d7207
Rohit Ninawe <rohitninawe1020@gmail.com>
2022-11-20 17:52:28 +0530

    feat: add email validation function

---
+++ index.js
@@ -0,0 +1,7 @@
+export default function isValidEmail(email) {
+    if (!email) {
+        throw "Param is not valid to check."
+    }
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return regex.test(email)
+}
